1a5bb306fcc737a9cfd03f1e5c29d22c3b47b167
Teo-Quijano <105201972+Teo-Quijano@users.noreply.github.com>
2022-07-23 23:22:08 +0100

    Actualizacion de Retos de Repaso

---
+++ main.js
@@ -0,0 +1,11 @@
+// Retos Repaso Main
+
+"use strict";
+exports.__esModule = true;
+var Punto_1 = require("./Punto");
+var triangulo_1 = require("./triangulo");
+var punto = new Punto_1.Punto(9, 3);
+var punto2 = new Punto_1.Punto(5, 2);
+var punto3 = new Punto_1.Punto(7, 5);
+var triangulo = new triangulo_1.Triangulo(punto, punto2, punto3);
+console.log(triangulo.calcularLongitudLados(punto, punto2, punto3));
---
+++ main.ts
@@ -0,0 +1,12 @@
+// Retos Repaso Main
+
+import { Punto } from "./Punto";
+import { Triangulo } from "./triangulo";
+
+let punto = new Punto(9, 3);
+let punto2 = new Punto(5, 2);
+let punto3 = new Punto(7, 5);
+
+let triangulo = new Triangulo(punto, punto2, punto3);
+
+console.log(triangulo.calcularLongitudLados(punto, punto2, punto3));
